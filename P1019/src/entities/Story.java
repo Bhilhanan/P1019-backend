19e0fffe39080584562852ac38c1fb5f189a7d7d
Bhilhanan A Jeyaram <bhilhanan@gmail.com>
2015-02-26 19:46:25 -0500

    initial commit

---
+++ P1019/src/entities/Story.java
@@ -0,0 +1,49 @@
+package entities;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType=IdentityType.APPLICATION)
+public class Story {
+
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
+	Long id;
+	@Persistent
+	String name;
+	@Persistent
+	String info;
+	@Persistent
+	String location;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+	
+	
+}
